c8d992ee74dec179e12671c14262c0fd4cdde9d2
Américo González <agzz2003@gmail.com>
2024-07-08 20:36:07 -0600

    Modificacion de la busqueda desde el programa. Busqueda dinamica

--- src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {//excepcion
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el nombre de la pelicula");
+        var busqueda = lectura.nextLine();
+
+        String direccion = "http://www.omdbapi.com/?apikey=48892d69&t="+busqueda;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()//obtener del servidor. Este es el builder de la peticion
-                .uri(URI.create("http://www.omdbapi.com/?apikey=48892d69&t=Avengers"))
+                .uri(URI.create(direccion))
                 .build();
 
         HttpResponse<String> response = client
